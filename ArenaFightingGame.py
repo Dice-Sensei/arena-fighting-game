51e1069de22a651001c365795c0d8aeb727ff29e
Erik Macháček <dice.sensei@gmail.com>
2025-11-05 09:00:10 +0100

    Add main file for a game - version from end of lecture 22.10.

---
+++ ArenaFightingGame.py
@@ -0,0 +1,127 @@
+# Malý projekt:
+# - začne se v hodině, dokončit doma
+# - Textová hra na hrdiny a jejich souboj v aréně - formát 1:1
+# - Programátor definuje hrdiny a ty budou mezi sebou bojovat
+# - Boj probíhá na kola, dokud jeden z oponentů nezemře
+#     - nutno kontrolovat na infinit loop
+#     - boj zprostředkovává aréna
+#     - soupeři na sebe nevidí napřímo (aby se nemohli ovlivňovat)
+# - Hero classes
+#     - warrior
+#     - mage
+#     - archer
+#     - healer
+#     - summoner?
+# - Skills
+#     - mají buď počet použití per fight
+#     - nebo cooldown
+#     - nebo casting time (wasting fight round)
+#     - nebo price (mana, stamina)
+# - princip fungování:
+#     - začne souboj
+#     - aréna představí oba soupeře
+##     - aréna refreshen oba soupeře (aby neměly vyplýtvaný skilly…)
+#     - začne kolo souboje
+#     - aréna na prvním vypočítá útok
+#     - aréna na druhém vypočítá obranu
+#     - aréna odečte na druhém životy
+#     - kontrola na úmrtnost
+#     - aréna na druhém vypočítá útok
+#     - aréna na prvním vypočítá obranu
+#     - aréna prvnímu odečte životy
+#     - kontrola na úmrtnost
+#     - konec kola
+# - STAT BLOCK (range 1-10?)
+#     - STR - ovlivňuje close attack
+#     - DEX - ovlivňuje long attack
+#     - END - defense
+#     - INT - spells
+#     - CHA - ???
+
+
+class Hero:
+    def __init__(self, name, stats, backstory=""):
+        self.name = name
+        self.stats = stats
+        self.backstory = backstory
+        self.health = 100
+
+    def introduce_yourself(self):
+        print(
+            f"I am {self.name} and my backstory is {"ups I dont have any" if self.backstory == "" else self.backstory}"
+        )
+        self.stats.print_values()
+
+    def calculate_attack_damage(self):
+        print("calculating attack damage...")
+        return self.stats.str
+
+    def calculate_defense(self):
+        print("calculating defense...")
+        return self.stats.end
+
+    def is_dead(self):
+        return self.health <= 0
+
+    def get_hurt(self, damage):
+        self.health -= damage
+        print(f"health after damage: {self.health}")
+
+
+class Stats:
+    def __init__(self, str, dex, end, int, cha):
+        self.str = str
+        self.dex = dex
+        self.end = end
+        self.int = int
+        self.cha = cha
+
+    def print_values(self):
+        print(f"Strength: {self.str}")
+        print(f"Dexterity: {self.dex}")
+        print(f"Endurance: {self.end}")
+        print(f"Intelligence: {self.int}")
+        print(f"Charisma: {self.cha}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    # define heroes
+    warrior = Hero("Sir Punchalot", Stats(8, 2, 5, 1, 4))
+    warrior.introduce_yourself()
+
+    mage = Hero("Just Old Merlin", Stats(2, 2, 2, 9, 8))
+    mage.introduce_yourself()
+
+    fighting_round = 1
+    # arena
+    while True:
+        print(f"Fighting round {fighting_round}")
+
+        warrior_damage = warrior.calculate_attack_damage()
+        mage_defense = mage.calculate_defense()
+
+        damage = warrior_damage - mage_defense
+        if damage > 0:
+            mage.get_hurt(damage)
+
+        # check death
+        if mage.is_dead():
+            print("mage is dead")
+            break
+
+        mage_damage = mage.calculate_attack_damage()
+        warrior_defense = warrior.calculate_defense()
+
+        damage = mage_damage - warrior_defense
+        if damage > 0:
+            warrior.get_hurt(damage)
+
+        # check death
+        if warrior.is_dead():
+            print("warrior is dead")
+            break
+
+        fighting_round += 1
+
+    print("Fight ended!")

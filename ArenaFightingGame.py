a4e5dbc610023d7f8bd6c1070d4c423bd19ea9ae
Dice-Sensei <dice.sensei@gmail.com>
2025-11-16 15:04:15 +0100

    Add a hero roster and move the definition of heroes there

--- ArenaFightingGame.py
+++ ArenaFightingGame.py
@@ -1,20 +1,9 @@
 from arena import Arena
-from hero.hero import Hero, Mage, Archer
-from stats import Stats
+from hero.hero_roster import warrior, archer
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    # define heroes
-    warrior = Hero("Sir Punchalot", Stats(8, 2, 5, 1, 4))
-    warrior.introduce_yourself()
-
-    mage = Mage("Just Old Merlin", Stats(2, 2, 2, 9, 8))
-    mage.introduce_yourself()
-
-    archer = Archer("Robin the Hood", Stats(2, 8, 4, 2, 7))
-    archer.introduce_yourself()
-
     arena = Arena()
     arena.register_heroes(warrior, archer)
     arena.introduce_heroes()
---
+++ hero/hero_roster.py
@@ -0,0 +1,9 @@
+﻿from hero.hero import Hero, Mage, Archer
+from stats import Stats
+
+# define heroes
+warrior = Hero("Sir Punchalot", Stats(8, 2, 5, 1, 4))
+
+mage = Mage("Just Old Merlin", Stats(2, 2, 2, 9, 8))
+
+archer = Archer("Robin the Hood", Stats(2, 8, 4, 2, 7))

0fd64902927bd624ee904adcb5cfbf67dcca8977
Erik Macháček <dice.sensei@gmail.com>
2025-11-19 12:07:18 +0100

    Add backstories for heroes

--- hero/hero_roster.py
+++ hero/hero_roster.py
@@ -4,8 +4,20 @@ from hero.warrior import Warrior
 from stats import Stats
 
 # define heroes
-warrior_punchalot = Warrior("Sir Punchalot", Stats(8, 2, 5, 1, 4))
+warrior_punchalot = Warrior(
+    "Sir Punchalot",
+    Stats(8, 2, 5, 1, 4),
+    "If punching people was discipline this would be a champion.",
+)
 
-mage_merlin = Mage("Just Old Merlin", Stats(2, 2, 2, 9, 8))
+mage_merlin = Mage(
+    "Just Old Merlin",
+    Stats(2, 2, 2, 9, 8),
+    "Once everywhere know for his intelligence and wisdom, now just old senile husk.",
+)
 
-archer_robin = Archer("Robin the Hood", Stats(2, 8, 4, 2, 7))
+archer_robin = Archer(
+    "Robin the Hood",
+    Stats(2, 8, 4, 2, 7),
+    "Taking from rich and giving everything to himself.",
+)

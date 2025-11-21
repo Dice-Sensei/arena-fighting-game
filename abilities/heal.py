aadde6766d38ccf37d5b85f414f07da1eeda8649
Erik Macháček <dice.sensei@gmail.com>
2025-11-21 11:25:33 +0100

    Add heal spell

---
+++ abilities/heal.py
@@ -0,0 +1,32 @@
+﻿from abilities.ability import Ability
+
+
+class Heal(Ability):
+    """
+    Heal
+    An ability to regenerate some health
+    """
+
+    def __init__(self):
+        super().__init__("Heal")
+
+        self.uses = 2
+        self.uses_left = self.uses
+
+    def is_ready(self):
+        return self.uses_left > 0
+
+    @staticmethod
+    def calculate_ability_strength(stats):
+        return 4 * stats.int
+
+    def use(self, stats):
+        print(f"using {self.name}")
+
+        self.uses_left -= 1
+
+        health_to_restore = self.calculate_ability_strength(stats)
+
+        print(f"calculating {self.name} healing: {health_to_restore}")
+
+        return health_to_restore
--- heros/priest.py
+++ heros/priest.py
@@ -1,7 +1,13 @@
 ﻿from heros.hero import Hero
+from abilities.heal import Heal
 
 
 class Priest(Hero):
+    def __init__(self, name, stats, backstory=""):
+        super().__init__(name, stats, backstory)
+
+        self.heal_spell = Heal()
+
     def calculate_attack_damage(self):
         attack = 1.0 * self.stats.int
 
@@ -9,9 +15,28 @@ class Priest(Hero):
 
         return attack
 
-    def calculate_defense(self):
+    def calculate_basic_defense(self):
+        print("using basic defense")
         defense = 0.9 * self.stats.end
 
         print(f"calculating defense: {defense}")
 
         return defense
+
+    def calculate_defense(self):
+        # check if healing is needed
+
+        max_health_health_difference = self.max_health - self.health
+
+        if (
+            self.heal_spell.is_ready()
+            and self.heal_spell.calculate_ability_strength(self.stats)
+            < max_health_health_difference
+        ):
+            old_health = self.health
+            self.health += self.heal_spell.use(self.stats)
+
+            print(f"healing myself from {old_health:.2f} to {self.health:.2f}")
+            return 0  # exposed
+
+        return self.calculate_basic_defense()
